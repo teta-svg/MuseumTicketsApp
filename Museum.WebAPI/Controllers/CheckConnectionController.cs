eea7900eb5d4412bef58f4f65db9b6676f0a15a8
teta-svg <prostonastja33@gmail.com>
2026-02-24 02:13:33 +0300

    БД подключена

---
+++ Museum.WebAPI/Controllers/CheckConnectionController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ConsoleApp17;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CheckConnectionController : ControllerBase
+{
+    private readonly MuseumTicketsDBContext _context;
+
+    public CheckConnectionController(MuseumTicketsDBContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        try
+        {
+            var canConnect = await _context.Database.CanConnectAsync();
+            if (canConnect)
+            {
+                return Ok("База данных подключена и работает.");
+            }
+            return BadRequest("База не отвечает. Проверь строку подключения.");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Ошибка подключения: {ex.Message}");
+        }
+    }
+}
--- Museum.WebAPI/Program.cs
+++ Museum.WebAPI/Program.cs
@@ -1,20 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using ConsoleApp17;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
+//бд
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MuseumTicketsDBContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
